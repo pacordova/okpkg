ccadbdccc4282161ab92bb80b253f881e0dbf888
pacordova <pac3nc@virginia.edu>
2023-01-29 08:32:27 +0000

    working on lua

--- functions.lua
+++ functions.lua
@@ -3,139 +3,207 @@ local sha2 = require 'sha2'
 unpack = table.unpack
 source_date_epoch = 1000000000
 
+local patches = "/usr/firepkg/patches"
+local sources  = "/usr/firepkg/sources"
+
 env = {
-    "SOURCE_DATE_EPOCH=" .. source_date_epoch
+  "SOURCE_DATE_EPOCH=" .. source_date_epoch
 }
 
 function paste(arr)
-    local s = ""
-    for i, v in ipairs(arr) do
-        s = s .. " " .. v
-    end
-    return s
+  local s = ""
+  for i, v in ipairs(arr) do
+    s = s .. " " .. v
+  end
+  return s
 end
 
 function ls(dir)
-    local cmd = {
-        "/usr/bin/ls",
-        dir,
-        "2>/dev/null"
-    }
-    local fd = io.popen(paste(cmd))
-    local arr = {}
-    for line in fd:lines() do
-        table.insert(arr, line)	
-    end
-    fd:close()
-    return arr
+  local cmd = {
+    "/usr/bin/ls",
+    dir,
+    "2>/dev/null"
+  }
+  local fd = io.popen(paste(cmd))
+  local arr = {}
+  for line in fd:lines() do
+    table.insert(arr, line)	
+  end
+  fd:close()
+  return arr
 end
 
 function strip(dir, ...)
-    local arr = ls(dir)
-    for _, file in ipairs(arr) do     
-        local cmd = {
-            "/usr/bin/strip",
-            ...,
-            file
-        }  
-        os.execute(paste(cmd))
-    end
+  local arr = ls(dir)
+  for _, file in ipairs(arr) do     
+    local cmd = {
+      "/usr/bin/strip",
+      ...,
+      file
+    }  
+    os.execute(paste(cmd))
+  end
 end
 
 function rm(file)
-    local cmd = {
-        "/usr/bin/rm",
-        "--recursive",
-        file,
-        "2>/dev/null"
-    }
-    os.execute(paste(cmd))
+  local cmd = {
+    "/usr/bin/rm",
+    "--recursive",
+    file,
+    "2>/dev/null"
+  }
+  os.execute(paste(cmd))
+end
+
+function clean(directory)
+  rm(directory)
+  local cmd = {
+    "/usr/bin/mkdir",
+    "--parents",
+    directory
+  }
+  os.execute(paste(cmd))
 end
 
 function compress(file)
-    local cmd = {
-        "/usr/bin/xz",
-        "--threads=0",
-        "--force",
-        file
-    }
-    os.execute(paste(cmd))
-    rm(file)
+  local cmd = {
+    "/usr/bin/xz",
+    "--threads=0",
+    "--force",
+    file
+  }
+  os.execute(paste(cmd))
+  rm(file)
 end
 
-function makepkg(dir)
-    --cleanup
-    strip(dir .. "/usr/lib64", "--strip-debug") 
-    strip(dir .. "/usr/bin",   "--strip-all")
-    strip(dir .. "/bin",       "--strip-all")
-    rm(dir .. "/usr/share/doc")
-    rm(dir .. "/usr/doc")
-    rm(dir .. "/usr/share/info")
 
-    --delete pyc files for reproducibility
-    local cmd = {
-        "/usr/bin/find",
-        ".",
-        "-name",
-        "'.pyc'",
-        "-delete"
-    }
-    os.execute(paste(cmd))
 
-    --make tarball
-    local cmd = {
-        "/usr/bin/tar",
-        "--directory",
-        dir,
-        "--sort=name",
-        "--mtime=@" .. source_date_epoch,
-        "--owner=0",
-        "--group=0",
-        "--numeric-owner",
-        "--create",
-        "--file",
-        dir .. ".tar",
-        unpack(ls(dir))
-    }
-    os.execute(paste(cmd))
-    
-    --compress
-    compress(dir .. ".tar")
-end
+function checksum(file, hash)
+  local fd = io.open(file)
+  local x = sha2.new256()
+  for b in fd:lines(2^12) do
+    x:add(b)
+  end
+  fd:close()
 
-function download(url)
-    local cmd = {
-        "/usr/bin/curl",
-        "--location",
-        "--remote-name",
-        url
-    }
-    os.execute(paste(cmd))
-end
+  verify = x:close() == hash
 
-function checksum(file)
-    local fd = io.open(file)
-    local x = sha2.new256()
-    for b in fd:lines(2^12) do
-        x:add(b)
-    end
-    fd:close()
-    return x:close()
+  if (verify) then
+    io.stderr:write("sha256sum: OK")
+  else
+    io.stderr:write("ERROR: checksum failed")
+  end
+
+  return verify
 end
 
 function vlook(pkgname)
-    local key = "^" .. pkgname .. " = "
-    local fd = io.open("example.lua", "r")
-    for line in fd:lines() do
-        if line:find(key) then
-            load("arr = " .. line:gsub(key, ""))()
-            break
-        end
+  local key = "^" .. pkgname
+  local fd = io.open("example.db", "r")
+  for line in fd:lines() do
+    if line:find(key) then
+      load(line:gsub(key, "arr"))()
+      break
     end
-    fd:close()
-    return arr
+  end
+  fd:close()
+  return arr
 end
+
+function extract(file, directory)
+  local cmd = {
+    "/usr/bin/tar",
+    "--directory=" .. directory,
+    "--strip-components=1",
+    "--extract",
+    "--file",
+    file
+  }
+  os.execute(paste(cmd))
+end
+
+function patch(patchfile, directory)
+  local fd = io.open(patchfile)
+  if (fd ~= nil) then
+    fd:close()
+    local cmd = {
+      "/usr/bin/patch",
+      "--directory=" .. directory,
+      "--strip=1",
+      "<" .. patchfile
+    }
+    os.execute(paste(cmd))
+  end
+end
+
+function download(pkgname)
+  local pkg       = vlook(pkgname)
+  local basename  = pkg.url:gsub("^.*/", "") 
+  local patchfile = "/usr/firepkg/patches/" .. pkgname .. ".diff"
+  local srcdir    = "/usr/firepkg/sources/" .. pkgname
+
+  local cmd = {
+    "/usr/bin/curl",
+    "--location",
+    "--remote-name",
+    pkg.url
+  }
+  os.execute(paste(cmd))
+
+  if not checksum(basename, pkg.hash) then
+    return -1
+  end
+
+  clean(srcdir)
+  extract(basename, srcdir)    
+  patch(pkgname, srcdir)
+  rm(basename)
+end
+
+function makepkg(dir)
+  --cleanup
+  strip(dir .. "/usr/lib64", "--strip-debug") 
+  strip(dir .. "/usr/bin",   "--strip-all")
+  strip(dir .. "/bin",       "--strip-all")
+  rm(dir .. "/usr/share/doc")
+  rm(dir .. "/usr/doc")
+  rm(dir .. "/usr/share/info")
+
+  --delete pyc files for reproducibility
+  local cmd = {
+    "/usr/bin/find",
+    ".",
+    "-name",
+    "'.pyc'",
+    "-delete"
+  }
+  os.execute(paste(cmd))
+
+  --make tarball
+  local cmd = {
+    "/usr/bin/tar",
+    "--directory",
+    dir,
+    "--sort=name",
+    "--mtime=@" .. source_date_epoch,
+    "--owner=0",
+    "--group=0",
+    "--numeric-owner",
+    "--create",
+    "--file",
+    dir .. ".tar",
+    unpack(ls(dir))
+  }
+  os.execute(paste(cmd))
+
+  --compress
+  compress(dir .. ".tar")
+end
+
+
+-----------------------------------------
 
 bash = vlook("bash")
 print(bash.build)
 
+download("bash")
