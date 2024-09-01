ecea200f7e736c8d56438b891115fc0b83f5e496
pac <pac3nc@virginia.edu>
2024-09-01 10:27:26 -0400

    printdeps

--- scripts/depends.sh
+++ scripts/depends.sh
@@ -2,12 +2,14 @@
 
 cd /usr/okpkg/index
 
-printlibs() {
+printlibs(){
     sed 's/^.//' $1.index | xargs file | \
     grep -e "executable" -e "shared object" | grep ELF | cut -f 1 -d : | \
     xargs objdump -p | awk '/NEEDED/{print $2}' | sort -u
 }
 
-for f in `printlibs xorg-server`; do
-    grep -rlie $f .
-done
+printdeps(){ 
+    for f in `printlibs $1`; do printf "%s: %s\n" $(grep -rlie $f .) $f; done
+}
+
+printdeps xorg-server
