5b56e66025a7e6bceb35563162982f10315e0c94
pac <pac3nc@virginia.edu>
2024-11-02 19:25:47 -0400

    fixes

--- scripts/download-all.lua
+++ scripts/download-all.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/env lua
 
 local ok = require("okutils")
-local mkdir = ok.mkdir
+local chdir, mkdir = ok.chdir, ok.mkdir
 local dbpath = "/var/lib/okpkg/db"
 local srcpath = "/var/lib/okpkg/sources"
 
@@ -9,10 +9,14 @@ dofile("/usr/bin/okpkg")
 
 local function download_all(db) 
    local fp, buf
-   fp = io.open(string.format("%s/%s, dbpath, db))
+   fp = io.open(string.format("%s/%s", dbpath, db))
    buf = '\n'..fp:read("*a")
    fp:close()
-   for i in buf:gmatch("\n([%w%-%+]-) = {.-;") do download(i) end
+   for i in buf:gmatch("\n([%w%-%+]-) = {.-;") do 
+      download(i) 
+      chdir(srcpath)
+      os.execute(string.format("rm -fr %s/%s", srcpath, i))
+   end
 end
 
 os.execute("rm -fr " .. srcpath)
