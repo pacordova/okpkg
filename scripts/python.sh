a59bfebf7c08c58f8ac9a47067beffa47eb586ef
pac <pac3nc@virginia.edu>
2024-12-23 13:50:11 -0500

    quick and dirty python rebuild script

---
+++ scripts/python.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -fr "/usr/lib64/python3"*
+okpkg purge python3
+okpkg emerge python3 
+okpkg emerge python-{flit-core,installer,pyproject-hooks,packaging,build,wheel,setuptools}
+okpkg emerge python-{markupsafe,jinja,docutils,mako,editables,typing_extensions,setuptools-scm}
+okpkg emerge python-{pluggy,pathspec,calver,trove-classifiers,hatchling,pygments,libevdev,pyudev,ply}
+okpkg emerge glad python-pyparsing cython pyyaml meson libxml2 xcb-proto
