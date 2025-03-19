7e4bd1e16c95159b9b884fba86a9547f83084321
pac <pac3nc@virginia.edu>
2025-03-19 18:56:22 -0400

    some meson stuff

--- scripts/build-system.lua
+++ scripts/build-system.lua
@@ -1,7 +1,6 @@
 #!/usr/bin/env lua
 
 local okpath = "/var/lib/okpkg"
-local dbpath = "/var/lib/okpkg/db"
 local pkgdir = "/var/lib/okpkg/packages"
 
 -- Ensure okpkg is installed
@@ -17,12 +16,18 @@ local chdir, mkdir, symlink =
 
 local fp, buf
 
+local function bootstrap_meson()
+   download("meson")
+   symlink(string.format("%s/sources/meson/meson.py", okpath), "/usr/bin/meson")
+   emerge("samurai")
+end
+
 -- Generate locales
 os.execute("localedef -i POSIX -f UTF-8 C.UTF-8 2> /dev/null || true")
 os.execute("localedef -i en_US -f UTF-8 en_US.UTF-8")
 
--- core system
-fp = io.open(string.format("%s/system.db", dbpath))
+-- Build core system
+fp = io.open(string.format("%s/db/system.db", okpath))
 buf = '\n' .. fp:read("*a")
 fp:close()
 for i in buf:gmatch("\n([%_%w%-%+]-) = {.-;") do
@@ -30,27 +35,27 @@ for i in buf:gmatch("\n([%_%w%-%+]-) = {.-;") do
       if not fp then emerge(i) else fp:close() end
    elseif i == "_perl" then fp = io.open("/usr/bin/perl")
       if not fp then emerge(i) else fp:close() end
-   elseif i:sub(1, 3) == "gcc" then
-      emerge(i)
+   elseif i == "kmod" or i == "dbus" then fp = io.open("/usr/bin/meson")
+      if not fp then bootstrap_meson() else fp:close() end emerge(i)
+   elseif i:sub(1, 3) == "gcc" then emerge(i)
       os.execute(string.format("%s/scripts/split-gcc.sh", okpath))
    else emerge(i) end
 end
 
--- rebuilds
+-- Rebuilds
 emerge("bison")
 emerge("gperf")
 emerge("glibc")
 emerge("binutils")
 emerge("mpfr")
 
--- misc post-install
-os.execute("makewhatis /usr/share/man")
-
--- fix versions and cleanup
+-- Fix versions and cleanup
 chdir(pkgdir)
 mkdir("a")
-os.rename("bash-5.2.32-amd64.tar.lz", "bash-5.2.032-amd64.tar.lz")
+purge("samurai")
+os.execute("makewhatis /usr/share/man")
+os.rename("bash-5.2.37-amd64.tar.lz", "bash-5.2.037-amd64.tar.lz")
 os.rename("readline-8.2.13-amd64.tar.lz", "readline-8.2.013-amd64.tar.lz")
-os.rename("sqlite-3460100-amd64.tar.lz", "sqlite-3.46.1-amd64.tar.lz")
+os.rename("sqlite-3490100-amd64.tar.lz", "sqlite-3.49.1-amd64.tar.lz")
 os.execute("mv *.tar.lz a")
-os.execute("rm -fr */_*.tar.lz")
+os.execute("rm -fr */_*.tar.lz */samurai-*.tar.lz")
