30b4a6c9f2d839bfa59e3473215ab74b2e707830
pac <pac3nc@virginia.edu>
2025-07-24 06:34:27 -0400

    updates

--- scripts/git-verify-tag.sh
+++ scripts/git-verify-tag.sh
@@ -27,7 +27,7 @@ ckone(){
         gzip -n > `basename $1`-$v.tar.gz
 }
 
-ckone https://github.com/vim/vim v9.1.1552
+ckone https://github.com/vim/vim v9.1.1585
 #ckone https://github.com/vcrhonek/hwdata v0.397
 #ckone https://github.com/yshui/picom v12.5
 #ckone https://github.com/eggert/tz 2025b
