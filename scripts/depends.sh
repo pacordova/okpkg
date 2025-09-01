916fde6b45bdb90c7a0eef9f079f86345e0ac7d9
pac <pac3nc@virginia.edu>
2025-08-31 21:42:35 -0400

    find errors

--- scripts/depends.sh
+++ scripts/depends.sh
@@ -2,16 +2,33 @@
 
 cd /var/lib/okpkg/index
 
+listlibs(){
+    sed 's/^.//' $1 | xargs file | \
+    grep -e "executable" -e "shared object" | grep ELF | cut -f 1 -d :
+}
+
 printlibs(){
-    sed 's/^.//' $1.index | xargs file | \
-    grep -e "executable" -e "shared object" | grep ELF | cut -f 1 -d : | \
-    xargs objdump -p | awk '/NEEDED/{print $2}' | sort -u
+    listlibs $1 | xargs objdump -p | awk '/NEEDED/{print $2}' | sort -u
+}
+
+notfound(){
+   sed 's/^.//' $1 | xargs file | \
+   grep -e "executable" -e "shared object" | grep ELF | cut -f 1 -d : \
+   xargs ldd | grep -e ":" -e "not found"
 }
 
 printdeps(){ 
-    for f in `printlibs $1`; do 
+    for f in `printlibs $1.index`; do 
         printf "%s: %s\n" $f $(grep -rlie $f . | sed 's:^\./::;s:\.index$::')
     done
 }
 
-printdeps xorg-server
+for i in /var/lib/okpkg/index/*.index; do
+  for l in `listlibs $i`; do
+    x=`ldd $l 2>/dev/null | grep "not found"`
+    if ! test -z "$x" ; then
+      printf "%s: %s: %s\n" "$i" "$l"
+      printf "%s\n" "$x"
+    fi
+  done
+done
