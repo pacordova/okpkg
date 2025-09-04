84468c3f8601f1eab8fdeb29f8bf04e69c65b436
pac <pac3nc@virginia.edu>
2025-09-04 05:31:42 -0400

    fix diffutils cross-compile, more filesystem stuff

--- scripts/filesystem.lua
+++ scripts/filesystem.lua
@@ -10,8 +10,16 @@ chdir("/mnt")
 -- Directory skeleton
 mkdir("boot")
 mkdir("boot/efi")
+mkdir("dev")
+mkdir("proc")
 mkdir("etc")
+mkdir("sys")
 mkdir("etc/opt")
+mkdir("etc/dinit.d")
+mkdir("etc/skel")
+mkdir("etc/ssl")
+mkdir("etc/ssl/certs")
+mkdir("etc/default")
 mkdir("etc/sysconfig")
 mkdir("mnt")
 mkdir("root")
@@ -20,6 +28,7 @@ mkdir("usr")
 mkdir("usr/bin")
 mkdir("usr/include")
 mkdir("usr/lib64")
+mkdir("usr/lib64/pkgconfig")
 mkdir("usr/lib64/locale")
 mkdir("usr/libexec")
 mkdir("usr/share")
@@ -29,6 +38,7 @@ mkdir("usr/share/doc")
 mkdir("usr/share/info")
 mkdir("usr/share/locale")
 mkdir("usr/share/man")
+mkdir("usr/src")
 mkdir("usr/share/man/man1")
 mkdir("usr/share/man/man2")
 mkdir("usr/share/man/man3")
@@ -54,6 +64,9 @@ mkdir("var/mail")
 mkdir("var/opt")
 mkdir("var/spool")
 mkdir("var/tmp")
+mkdir("run")
+mkdir("run/lock")
+mkdir("run/shm")
 
 -- Symlinks
 symlink("lib64", "usr/lib")
@@ -64,11 +77,25 @@ symlink("usr/lib64", "lib64")
 symlink("usr/lib", "lib")
 symlink("var/home", "home")
 symlink("/proc/self/mounts", "etc/mtab")
+symlink("/var/lib/okpkg/sources/linux-lts", "usr/src/linux")
 
 -- Files
--- etc/hosts
--- etc/passwd
+-- etc/config.site
+-- etc/default/useradd
 -- etc/group
+-- etc/hostname
+-- etc/hosts
+-- etc/inputrc
+-- etc/ld.so.conf
+-- etc/nsswitch.conf
+-- etc/passwd
+-- etc/profile
+-- etc/resolv.conf
+-- etc/shells
+-- etc/syslog.conf
+-- dev/console
+-- dev/null
+--
 os.execute [[
    touch var/log/{btmp,lastlog,faillog,wtmp}
    chgrp utmp /var/log/lastlog
