54cf7eeccf78561285abc58e8359f847384bc8ab
pac <pac3nc@virginia.edu>
2025-09-07 07:30:31 -0400

    adjust

--- scripts/system.lua
+++ scripts/system.lua
@@ -14,40 +14,34 @@ local ok = require("okutils")
 local chdir, mkdir, symlink =
    ok.chdir, ok.mkdir, ok.symlink
 
-local fp, buf
-
-local function bootstrap_meson()
-   download("meson")
-   symlink(string.format("%s/sources/meson/meson.py", okpath), "/usr/bin/meson")
-   emerge("samurai")
-end
+local file, buf
 
 -- Generate locales
 os.execute("localedef -i POSIX -f UTF-8 C.UTF-8 2> /dev/null || true")
 os.execute("localedef -i en_US -f UTF-8 en_US.UTF-8")
 
+-- Bootstrap meson
+download("meson")
+symlink(string.format("%s/sources/meson/meson.py", okpath), "/usr/bin/meson")
+emerge("samurai")
+
 -- Build core system
-fp = io.open(string.format("%s/db/system.db", okpath))
-buf = '\n' .. fp:read("*a")
-fp:close()
+file = io.open(string.format("%s/db/system.db", okpath))
+buf = '\n' .. file:read("*a")
+file:close()
 for i in buf:gmatch("\n([%_%w%-%+]-) = {.-;") do
-   if i == "_python" then fp = io.open("/usr/bin/python3")
-      if not fp then emerge(i) else fp:close() end
-   elseif i == "_perl" then fp = io.open("/usr/bin/perl")
-      if not fp then emerge(i) else fp:close() end
-   elseif i == "kmod" or i == "dbus" then fp = io.open("/usr/bin/meson")
-      if not fp then bootstrap_meson() else fp:close() end emerge(i)
-   elseif i:sub(1, 3) == "gcc" then emerge(i)
+   emerge(i)
+   if i:sub(1, 3) == "gcc" then 
       os.execute(string.format("%s/scripts/split-gcc.sh", okpath))
-   else emerge(i) end
+   end
 end
 
 -- Rebuilds
-emerge("bison")
-emerge("gperf")
-emerge("glibc")
-emerge("binutils")
-emerge("mpfr")
+--emerge("bison")
+--emerge("gperf")
+--emerge("glibc")
+--emerge("binutils")
+--emerge("mpfr")
 
 -- Fix versions and cleanup
 chdir(pkgdir)
