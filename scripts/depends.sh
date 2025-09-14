6905d07abe4908329483cc7453d8d75c11c949e4
pac <pac3nc@virginia.edu>
2025-09-14 16:24:30 -0400

    bump versions

--- scripts/depends.sh
+++ scripts/depends.sh
@@ -25,7 +25,7 @@ printdeps(){
 
 for i in /var/lib/okpkg/index/*.index; do
   for l in `listlibs $i`; do
-    x=`ldd $l 2>/dev/null | grep "librsvg"`
+    x=`ldd $l 2>/dev/null | grep "not found"`
     if ! test -z "$x" ; then
       printf "%s: %s: %s\n" "$i" "$l"
       printf "%s\n" "$x"
