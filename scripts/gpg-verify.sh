9dae46f025ef25805d03309d3aed8110d9cf5564
pac <pac3nc@virginia.edu>
2025-10-04 16:39:10 -0400

    bumps

--- scripts/gpg-verify.sh
+++ scripts/gpg-verify.sh
@@ -25,9 +25,9 @@ _cksum_signal(){
 
 cd "$tempdir"
 
-#_cksum_signal
-_cksum_gh https://github.com/vim/vim v9.1.1797
-#_cksum_gh https://github.com/vcrhonek/hwdata v0.399
+_cksum_signal
+_cksum_gh https://github.com/vim/vim v9.1.1825
+_cksum_gh https://github.com/vcrhonek/hwdata v0.400
 #_cksum_gh https://github.com/yshui/picom v12.5
 #_cksum_gh https://github.com/eggert/tz 2025b
 #_cksum_gh https://github.com/pnggroup/libpng v1.6.44
