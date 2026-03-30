8c1424e297f7dde47f6298d0de8950289d6f03a4
pac <pac3nc@virginia.edu>
2026-03-30 00:03:47 -0400

    better

--- src/remove_all.cc
+++ src/remove_all.cc
@@ -1,12 +1,15 @@
 #include <filesystem>
 
 extern "C" {
-    #include <lauxlib.h>
 
-    int
-    ok_remove_all(lua_State *L) {
-	const std::filesystem::path p = luaL_checkstring(L, 1);
-	lua_pushinteger(L, std::filesystem::remove_all(p));
-	return 1;
-    }
+#include <lauxlib.h>
+
+int
+ok_remove_all(lua_State *L) 
+{
+    const std::filesystem::path p = luaL_checkstring(L, 1);
+    lua_pushinteger(L, std::filesystem::remove_all(p));
+    return 1;
+}
+
 }
