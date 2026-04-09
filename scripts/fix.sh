96d155713f8471e993a2e4e95c51d6460150b275
pac <pac3nc@virginia.edu>
2026-04-09 15:04:00 -0400

    better script

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-# Various fixes
-makewhatis /usr/share/man;
-pwconv && grpconv;
-glib-compile-schemas /usr/share/glib-2.0/schemas;
-fc-cache;
-update-desktop-database;
-update-mime-database /usr/share/mime;
-gdk-pixbuf-query-loaders --update-cache;
-setcap cap_net_raw+p /usr/bin/ping;
-chown root:messagebus /usr/lib64/dbus-daemon-launch-helper;
-chmod 4750 /usr/lib64/dbus-daemon-launch-helper;
+# update manpages
+makewhatis /usr/share/man
 
-# fix date
-date +'%y-%m-%d %H:%M:%S' -us"$(nc time.nist.gov 13|awk 'NR>1{print$2" "$3}')"
+# update font cache
+fc-cache
+
+# desktop and mime databases
+update-desktop-database
+update-mime-database /usr/share/mime
+
+# glib schemas
+glib-compile-schemas /usr/share/glib-2.0/schemas
+
+# pixbuf loaders
+gdk-pixbuf-query-loaders --update-cache
+
+# commit any user changes
+pwconv && grpconv
+
+# ping capabilities
+setcap cap_net_raw+p /usr/bin/ping
+
+# dbus permissions
+chown root:messagebus /usr/lib64/dbus-daemon-launch-helper
+chmod 4750 /usr/lib64/dbus-daemon-launch-helper
+
+# clock
+date -u $(nc time.nist.gov 13 | awk -F'[-: ]' 'NR>1{print $3$4$5$6$2"."$7}')
 hwclock --systohc && hwclock --hctosys
