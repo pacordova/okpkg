45ffa6efdbc55d5d823243e13e0c9cde726e7adc
pac <pac3nc@virginia.edu>
2026-04-17 00:02:13 -0400

    updates

--- scripts/gpg-verify.sh
+++ scripts/gpg-verify.sh
@@ -38,7 +38,7 @@ _cksum_signal(){
 
 cd "$tempdir"
 
-_cksum_signal 8.6.1
+_cksum_signal 8.7.0
 #_cksum_gl https://gitlab.com/lvmteam/lvm2 v2_03_39
 #_cksum_gh https://github.com/vim/vim v9.1.1980
 #_cksum_gh https://github.com/vcrhonek/hwdata v0.406
