475caff611bff581ee86dd47c2d6e90c44a8e7c1
pac <pac3nc@virginia.edu>
2026-04-18 11:58:10 -0400

    fmt

--- src/directory_iterator.cc
+++ src/directory_iterator.cc
@@ -11,14 +11,21 @@ ok_directory_iterator(lua_State *L)
 {
     const fs::path p = luaL_checkstring(L, 1);
     static fs::directory_iterator i;
-    if (i == fs::end(i))
+
+    if (i == fs::end(i)) {
         i = fs::directory_iterator(p);
+    }
+
     lua_pushcfunction(L, [](lua_State *L) {
-        if (i == fs::end(i))
+        if (i == fs::end(i)) {
             return 0;
-        lua_pushstring(L, i->path().c_str());
-        ++i;
-        return 1;
+        }
+        else {
+            lua_pushstring(L, i->path().c_str());
+            ++i;
+            return 1;
+        }
     });
+
     return 1;
 }
