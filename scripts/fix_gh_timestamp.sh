a1f34eff41d0058744394296a028ea552c93e07e
pac <pac3nc@virginia.edu>
2026-05-31 03:25:01 -0400

    fix_gh_timestamp

---
+++ scripts/fix_gh_timestamp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fix_gh_timestamp(){
+   ymd=;SOURCE_DATE_EPOCH=;
+   ymd=$(tar --full-time -tvf "$1" | awk '{print $4" "$5;exit}')
+   SOURCE_DATE_EPOCH=$(date -d "$ymd" +%s)
+   touch -hd "@$SOURCE_DATE_EPOCH" "$1"
+}
+
