28a4dedfd97045d1c5a5428a8cda5ebc5fbe3ebc
pacordova <pac2198@columbia.edu>
2026-06-23 08:57:56 -0400

    opendoas

--- scripts/depends.sh
+++ scripts/depends.sh
@@ -59,8 +59,8 @@ unsafe(){
 }
 
 # Traditional but unsafe
-#for f in $indexdir/*; do unsafe < $f | sed "s|^|[$f]: |"; done
+for f in $indexdir/*; do unsafe < $f | sed "s|^|[$f]: |"; done
 
-mkcache
+#mkcache
 #revdeps eudev
 #revdeps upower
