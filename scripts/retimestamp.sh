46f189d657f58b7ce4b4fbeac4177dc400e630d2
pacordova <pac2198@columbia.edu>
2026-07-01 02:17:50 -0400

    rename

--- scripts/retimestamp.sh
+++ scripts/retimestamp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fix_gh_timestamp(){
+retimestamp(){
    ymd=;SOURCE_DATE_EPOCH=;
    ymd=$(tar --full-time -tvf "$1" | awk '{print $4" "$5;exit}')
    SOURCE_DATE_EPOCH=$(date -d "$ymd" +%s)
