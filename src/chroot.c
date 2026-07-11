bbd5d2368dd5e41e1b6098b8dcad3247d0eb8881
pacordova <pac2198@columbia.edu>
2026-07-11 19:48:16 -0400

    no mkdir

--- src/chroot.c
+++ src/chroot.c
@@ -17,21 +17,15 @@ ok_chroot(lua_State *L)
 {
     const char *path = luaL_checkstring(L, 1);
     char *const cmd[] = {"/bin/sh", "-i", NULL};
-//    const char *term = (const char *)getenv("TERM");
 
     if (chdir(path)) {
         lua_pushinteger(L, -1);
         return 1;
     }
 
-    if (access("./proc", F_OK)) mkdir("./proc", 0555);
-    mount("/proc", "proc", "proc", MS_NOEXEC|MS_NODEV|DEFAULT, "hidepid=1");
-
-    if (access("./sys", F_OK)) mkdir("./sys", 0555);
-    mount("/sys", "sys", "sysfs", MS_NOEXEC|MS_NODEV|DEFAULT, NULL);
-
-    if (access("./dev", F_OK)) mkdir("./dev", 0755);
-    mount("/dev", "dev", "devtmpfs", DEFAULT, NULL);
+    mount("/proc", "proc", "proc",     MS_NOEXEC|MS_NODEV|DEFAULT, "hidepid=1");
+    mount("/sys",  "sys",  "sysfs",    MS_NOEXEC|MS_NODEV|DEFAULT, NULL);
+    mount("/dev",  "dev",  "devtmpfs", DEFAULT,                    NULL);
 
     if (chroot(".") || chdir("/") || execvp(cmd[0], cmd)) {
         lua_pushinteger(L, -1);
